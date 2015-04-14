261f8fba1a6d3c23c184f10b18bb3f615015f9d6
simacna <simacna@gmail.com>
2015-04-14 16:26:20 -0400

    going through examples

--- Ch 2/variableScope.js
+++ Ch 2/variableScope.js
@@ -1,1 +1,26 @@
 //Notes taken on ch. 2 - variable Scope
+
+//Ex. 1
+
+//This example shows using the same variables i,n,sum inside the scope of
+//each function 
+//questions: 1) in the score function, what is player.levels.length? I'll google this
+
+function averageScores(players) {
+	var i, n, sum; 
+	sum = 0;
+	for (i = 0, n = players.length; i < n; i++)  {
+		sum += score(players[i]);
+	}
+	return sum / n;
+}
+
+function score(player) {
+	var i, n, sum; 
+	sum = 0;
+	for (i = 0, n = player.levels.length; i < n; i++) {
+		sum += player.levels[i].score;
+	}
+
+	return sum;
+}
