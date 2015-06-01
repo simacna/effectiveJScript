5dbaacb5195b41ff43ad963549bea8183621a186
simacna <simacna@gmail.com>
2015-06-01 14:39:54 -0400

    continue ch 2

--- Ch 2/variableScope.js
+++ Ch 2/variableScope.js
@@ -198,6 +198,42 @@ function isWinner(player, others){
 	}
 }
 
+//the above function is equal to the function below due to variable hoisting
+
+function trimSections(header, body, footer){
+	var i, n;
+	
+	for (i = 0, n = header.length; i < n; i++) {
+		header[i] = header[i].trim();
+	}
+	for (var i, n = body.length; i < n; i++) {
+		body[i] = body[i].trim();
+	}
+	for(var i = 0, n = footer.length; i++){
+		footer[i] = footer[i].trim();
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
