3685e0320b684190d2ea681b93ab77fccec17f74
simacna <simacna@gmail.com>
2015-06-19 12:50:38 -0400

    finishing ch. 3.36

--- Ch.4/objects.js
+++ Ch.4/objects.js
@@ -152,6 +152,57 @@ function User(name, passwordHash){
 // 1. Closure variables are private, accessible only to local references
 // 2. Use local variables as private data to enforce information hiding within methods
 
+Item 3.36 - store instances state only on instance objects 
+Store per-instance data on the instance object and not the prototype - aka if data is mutable, place it on the instance object. Example below:
+
+the below is incorrect:
+
+function Tree(x){
+	this.value = x;
+}
+
+Tree.prototype = {
+	children = [],
+	addChild: function(x){
+		this.children.push(x)
+	}
+};
+
+consider what happens when we try to construct a tree with this class:
+
+var left = new Tree(2);
+left.addChild(1);
+left.addChild(3);
+
+var right = new Tree(6);
+right.addChild(5);
+right.addChild(7);
+
+var top = new Tree(4);
+top.addChild(left);
+top.addChild(right);
+
+top.children; //[1,3,5,7,left, right]
+
+Each time we call addChild, we append a value to Tree.prototype.children, which contains the nodes in the order of any calls to addChild anywhere! This leaves the Tree
+objects in the incoherent state showin in 4.5.
+
+The correct way to implement the Tree class is to create a separate children array for each instance object:
+
+function Tree(x){
+	this.value = x;
+	this.children = []; //instance state
+}
+
+Tree.prototype = {
+	addChild: function(x){
+		this.children.push(x);
+	}
+}
+
+
+
+
 
 
 
